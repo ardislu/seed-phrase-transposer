438e0364ff203efa6d22028f852319dbac6feec5
Ardis Lu <ardisglu@gmail.com>
2025-05-02 23:47:48 -0700

    Upgrade dependencies

--- main_test.ts
+++ main_test.ts
@@ -5,7 +5,7 @@ import {
   assertEquals,
   assertFalse,
   assertThrows,
-} from "jsr:@std/assert@1.0.12";
+} from "jsr:@std/assert@1.0.13";
 import { assertSpyCalls, spy, stub } from "jsr:@std/testing@1.0.11/mock";
 
 Deno.test("parseInput throws on null", () => {
