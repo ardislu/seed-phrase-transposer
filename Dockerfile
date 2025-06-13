c9024c7968331bb6a58e66250fe3947224c65445
Ardis Lu <ardisglu@gmail.com>
2025-06-12 23:23:47 -0700

    Upgrade dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM denoland/deno:alpine-2.3.5
+FROM denoland/deno:alpine-2.3.6
 USER deno
 # Instantiate analysis cache to avoid console warnings later when running this image with the --read-only flag
 RUN deno
--- main_test.ts
+++ main_test.ts
@@ -6,7 +6,7 @@ import {
   assertFalse,
   assertThrows,
 } from "jsr:@std/assert@1.0.13";
-import { assertSpyCalls, spy, stub } from "jsr:@std/testing@1.0.13/mock";
+import { assertSpyCalls, spy, stub } from "jsr:@std/testing@1.0.14/mock";
 
 Deno.test("parseInput throws on null", () => {
   assertThrows(() => parseInput(null));
